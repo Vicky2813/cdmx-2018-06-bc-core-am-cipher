3a00c5598dcdce7539db4ec22860b643c9afd391
Victoria Acosta <vicky.acostahidalgo@gmail.com>
2018-06-07 15:24:57 -0500

    primera parte del cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,31 @@
-window.cipher = {
-  // ...
-};
+// FUNCION PARA CIFRAR
+  // RECIBE (numero de deplazamientos, string a cifrar)
+window.cipher = (n, string) => {
+  // result VA A CONTENER MI RESULTADO FINAL
+  let result = ''
+
+  // RECORRO LA FRASE PARA CIFRAR
+  for (let i = 0; i < string.length; i++) {
+    // PARA CADA LETRA DE LA FRASE APLICO LA FORMULA
+    result += String.fromCharCode((((string.charCodeAt(i) - 65) + n) % 26) + 65)
+  }
+
+  // RETORNO EL RESULTADO
+  return result
+}
+
+// FUNCION PARA CIFRAR
+  // RECIBE (numero de deplazamientos, string a decifrar)
+window.descipher = (n, string) => {
+  // result VA a CONTENER MI RESULTADO FINAL
+  let result = ''
+
+  // RECORRO LA FRASE POR CADA LETRA PARA DESCIFRAR
+  for (let i = 0; i < string.length; i++) {
+    // PARA CADA LETRA APLICO LA FORMULA A LA INVERSA
+    result += String.fromCharCode((((string.charCodeAt(i) - 65) - n) % 26) + 65)
+  }
+
+  // RETORNO EL RESULTADO
+  return result
+}
---
+++ src/index.js
@@ -0,0 +1,52 @@
+// DECLACARACIONES
+
+// REFERENCIA AL FORMULARIO DE CIFRADO
+const formCipher = document.getElementById('formCipher');
+
+// REFERENCIA A FORMULARIO DE DECIFRADO
+const formDecipher = document.getElementById('formDecipher');
+
+// REFERENCIA AL INPUT DE LA PALABRA A ENCRIPTAR
+const inputWordToEncrypt = document.getElementById('wordToEncrypt');
+
+// REFERENCIA AL INPUT QUE TIENE EL NUMERO DE DESPLAZAMIENTOS A APLICAR
+const shiftNumber = document.getElementById('shift');
+
+// REFERENCIA AL INPUT QUE CONTIENE LA PALABARA A DESENCRIPTAR
+const inputWordToDecrypt = document.getElementById('wordToDeCrypt');
+
+// EVENTO DE SUBMIT PARA EL FORMULARIO DE CIFRADO
+formCipher.addEventListener('submit', (event) => {
+  // PREVENGO QUE SE RECARGUE LA PAGINA
+  event.preventDefault();
+
+  // OBTENGO EL NUMERO DESPLAZAMIENTOS DEL INPUT
+  const n = parseInt(shiftNumber.value);
+
+  // OBTENGO LA FRASE A ENCRIPTAR
+  const phraseToEncrypt = inputWordToEncrypt.value;
+
+  // LLAMO A LA FUNCION GLOBAL "cipher" EN window PARA ENCRIPTAR LA FRASE
+  const result = window.cipher(n, phraseToEncrypt);
+
+  // COLOCO EL RESULTADO DEL CIFRADO EN EL INPUT PARA DESCIFRAR
+  inputWordToDecrypt.value = result
+});
+
+// EVENTO DE SUBMIT PARA EL FORMULARIO DE DECIFRADO
+formDecipher.addEventListener('submit', (event) => {
+  // PREVENGO QUE SE RECARGUE LA PAGINA
+  event.preventDefault();
+
+  // OBTENGO EL NUMERO DE DESPLAZAMIENTOS A APLICAR
+  const n = parseInt(shiftNumber.value);
+
+  // OBTENGO LA FRASE A DESENCRIPTAR
+  const phraseToDecrypt = inputWordToDecrypt.value;
+
+  // LLAMO A LA FUNCION GLOBAL "decipher" EN window PARA DESENCRIPTAR
+  const result = window.descipher(n, phraseToDecrypt)
+
+  // COLOCO EL RESULTADO EN EL INPUT DE CRIFRAR
+  inputWordToEncrypt.value = result
+});
