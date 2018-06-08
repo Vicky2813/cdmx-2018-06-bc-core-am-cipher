6801aeaca7fe51c3cab5b909e4bdc7e4743466f1
Victoria Acosta <vicky.acostahidalgo@gmail.com>
2018-06-07 23:09:41 -0500

    Aceptando caracteres en minusculas y soportando espacios en blanco

--- src/cipher.js
+++ src/cipher.js
@@ -2,28 +2,54 @@
   // RECIBE (numero de deplazamientos, string a cifrar)
 window.cipher = (n, string) => {
   // result VA A CONTENER MI RESULTADO FINAL
+
   let result = ''
 
   // RECORRO LA FRASE PARA CIFRAR
   for (let i = 0; i < string.length; i++) {
-    // PARA CADA LETRA DE LA FRASE APLICO LA FORMULA
-    result += String.fromCharCode((((string.charCodeAt(i) - 65) + n) % 26) + 65)
+    // SI NO ES UNA LETRA EN MINUSCULAS O MAYUSCULAS
+    if (!(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) &&  !(string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) ){
+      // SOLAMENTE LO AGREGO EL CARACTER SIN CIFRAR
+      result = result + string.charAt(i)
+    } else {
+      // PARA CIFRAR MAYUSCULAS (65 - 90)
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        result += String.fromCharCode((((string.charCodeAt(i) - 65) + n) % 26) + 65)
+
+        // PARA CIFRAR MINUSCULAS (97 - 122)
+      } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        result += String.fromCharCode((((string.charCodeAt(i) - 122) + n) % 26) + 122)
+      }
+    }
   }
 
   // RETORNO EL RESULTADO
   return result
 }
 
-// FUNCION PARA CIFRAR
+// FUNCION PARA DECIFRAR
   // RECIBE (numero de deplazamientos, string a decifrar)
 window.descipher = (n, string) => {
-  // result VA a CONTENER MI RESULTADO FINAL
+  // result VA A CONTENER MI RESULTADO FINAL
+
   let result = ''
 
-  // RECORRO LA FRASE POR CADA LETRA PARA DESCIFRAR
+  // RECORRO LA FRASE PARA DECIFRAR
   for (let i = 0; i < string.length; i++) {
-    // PARA CADA LETRA APLICO LA FORMULA A LA INVERSA
-    result += String.fromCharCode((((string.charCodeAt(i) - 65) - n) % 26) + 65)
+    // SI NO ES UNA LETRA EN MINUSCULAS O MAYUSCULAS
+    if (!(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) &&  !(string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) ){
+      // SOLAMENTE LO AGREGO EL CARACTER SIN DECIFRAR
+      result = result + string.charAt(i)
+    } else {
+      // PARA DECIFRAR MAYUSCULAS (65 - 90)
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        result += String.fromCharCode((((string.charCodeAt(i) - 65) - n) % 26) + 65)
+
+        // PARA DECIFRAR MINUSCULAS (97 - 122)
+      } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        result += String.fromCharCode((((string.charCodeAt(i) - 122) - n) % 26) + 122)
+      }
+    }
   }
 
   // RETORNO EL RESULTADO
